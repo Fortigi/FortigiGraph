1e53235216aa36c5134ae809d7b71ec047c9146d
Wim van den Heijkant <wim@fortigi.nl>
2024-01-02 16:17:11 +0100

    Fix removing sleep

--- Generic/New-FGAccessPackagePolicy.ps1
+++ Generic/New-FGAccessPackagePolicy.ps1
@@ -9,9 +9,6 @@ function New-FGAccessPackagePolicy {
 
     $URI = "https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageAssignmentPolicies"
     $Body = $Policy 
-    
-    #It takes a little time before a group can be added to a policy.. so sleep..
-    Start-sleep -s 45
 
     $ReturnValue = Invoke-FGPostRequest -URI $URI -Body $Body
     return $ReturnValue
