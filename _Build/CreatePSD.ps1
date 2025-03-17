abdeaf5014e3ae2b678551920519f24789040252
Wim van den Heijkant <wim@fortigi.nl>
2025-03-17 10:39:20 +0100

    Update module version, as published

--- _Build/CreatePSD.ps1
+++ _Build/CreatePSD.ps1
@@ -3,7 +3,7 @@ $Company = "Fortigi"
 $Description = "PowerShell Module to assist with scripting against the Microsoft Graph. The sources for this module, including versioning can be found on GitHub: https://github.com/Fortigi/FortigiGraph"
 
 $VersionMajor = "1"
-$VersionMinor = "0"
+$VersionMinor = "1"
 $Version = $VersionMajor + "." + $VersionMinor + "." + (Get-Date -Format "yyyyMMdd") + "." + (Get-Date -Format "HHmm")
 
 $Path = "C:\Source\Fortigi\GitHub\FortigiGraph"
